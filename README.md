dbd5377c0f3839d2e54119768aa4d575e145d86d
Olga <helga_ser@mail.ru>
2023-10-29 18:01:35 +0300

    Test Positive

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/w4386e891m7n6els?svg=true)](https://ci.appveyor.com/project/Helga52/at-dz3)
---
+++ src/test/java/ru/netology/javaqa/FormCardPositiveTest.java
@@ -0,0 +1,52 @@
+package ru.netology.javaqa;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class FormCardPositiveTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void SetupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldFormFieldsCorrect() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Петров Николай Степанович");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79105554322");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+
+
+    }
+
+}
